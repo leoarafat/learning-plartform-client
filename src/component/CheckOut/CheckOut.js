39b2160585a0a91fa9603af2a2c736f048593adf
leoarafat10 <97153759+leoarafat10@users.noreply.github.com>
2022-10-26 21:20:50 +0600

    update checkout page

--- src/component/CheckOut/CheckOut.js
+++ src/component/CheckOut/CheckOut.js
@@ -1,5 +1,6 @@
 import React, { useContext, useRef, useState } from "react";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
+import swal from "sweetalert";
 import { AuthContext } from "../Context/UserContext";
 
 const CheckOut = () => {
@@ -19,6 +20,11 @@ const CheckOut = () => {
   const handleNameChange = (event) => {
     setUserName(event.target.value);
   };
+  const navigate = useNavigate()
+  const handleCheckout = () =>{
+navigate('/userProfile')
+swal("Submitted!", "You can visit now!", "success");
+  }
 
   return (
     <div>
@@ -29,8 +35,8 @@ const CheckOut = () => {
           action=""
           className="flex flex-col w-full max-w-lg p-12 rounded shadow-lg dark:text-gray-100 ng-untouched ng-pristine ng-valid"
         >
-          <p>{name}</p>
-          <p>{title}</p>
+          <p className="bg-indigo-500 mb-1">{name}</p>
+          <p className="bg-indigo-500">{title}</p>
           <label for="email" className="self-start text-xs font-semibold">
             Email
           </label>
@@ -69,6 +75,7 @@ const CheckOut = () => {
           />
 
           <button
+          onClick={handleCheckout}
             type="submit"
             className="flex items-center justify-center h-12 px-6 mt-8 text-sm font-semibold rounded dark:bg-violet-400 dark:text-gray-900"
           >
